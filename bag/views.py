7bcd7373e9efb09ade93d14b0d91a5c564b6a21d
Ubuntu <ubuntu@ip-172-31-42-132.eu-west-2.compute.internal>
2020-06-13 14:09:10 +0000

    shopping bag template/urls added

---
+++ bag/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def view_bag(request):
+    """ A View to return the bag page"""
+    
+    return render(request, 'bag/bag.html')
